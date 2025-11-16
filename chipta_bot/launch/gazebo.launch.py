6c257588ace5da4c3af0a374ebeb29ff4199ea98
iAdityaDev <adityadevsingh16@gmail.com>
2025-11-16 22:46:41 +0530

    spawned

--- chipta_bot/launch/gazebo.launch.py
+++ chipta_bot/launch/gazebo.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
 
     # Package Directories
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_ros_gz_rbot = get_package_share_directory('chipta_description')
+    pkg_ros_gz_rbot = get_package_share_directory('chipta_bot')
 
     # Parse robot description from xacro
     robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'chipta.xacro')
--- chipta_bot/setup.py
+++ chipta_bot/setup.py
@@ -13,7 +13,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
 
-        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
